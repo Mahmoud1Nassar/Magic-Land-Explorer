70ba1f55cc4c0e85f6754d54fce15a899b547ab3
mhmooud nassar <mhmooud456@gmail.com>
2024-07-26 21:41:17 +0300

    Solved

---
+++ Magic-Land-Explorer/Destination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Land_Explorer
+{
+    internal class Destination
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Duration { get; set; }
+        public string Description { get; set; }
+        public string ShowTime { get; set; }
+    }
+}
